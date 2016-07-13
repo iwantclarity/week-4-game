fa8462f7bcbe66e97ea18a8afb548f83400ad6ef
David Forman <djforman@gmail.com>
2016-07-13 19:24:00 -0400

    Added function to reset game

---
+++ assets/javascript/game.js
@@ -0,0 +1,65 @@
+
+  $( document ).ready(function() {
+
+    var numberToGuess = Math.floor(Math.random() * 120) + 19; //First set the number for the user to guess
+
+    var numbersOnCrystals = []; //Define the array to store the numbers for the crystals
+
+    var counter = 0; //Set the counter that will be added to by the user
+
+        
+        //The function makes sure that at the start and at the end of games the numbers are reset
+        function NumberReset() {   
+
+        var numbersOnCrystals = [];
+
+        for (i=0; i < 4; i++) {
+          var numberToArray = Math.floor(Math.random() * 12) + 1;
+          numbersOnCrystals.push(numberToArray);
+        }
+
+        counter = 0;
+
+        $('#yourNumber').text(counter);
+
+        }
+
+    NumberReset();
+
+    console.log(numbersOnCrystals);
+
+    $('#number').text(numberToGuess);
+
+    var picArray = ["assets/images/pic1.jpg", "assets/images/pic2.jpg", "assets/images/pic3.jpg","assets/images/pic4.jpg"];
+
+    for (var i=0; i< numbersOnCrystals.length; i++){
+
+      var imageCrystal = $('<img>');
+
+      imageCrystal.attr('data-num', numbersOnCrystals[i]);
+
+      imageCrystal.attr('src', picArray[i]);
+
+      imageCrystal.attr('alt', 'crystals');
+
+      imageCrystal.addClass('crystalImage');
+
+      $('#crystals').append(imageCrystal);
+    }
+
+    $('.crystalImage').on('click', function(){
+      counter = counter + parseInt($(this).data('num'));
+
+      $('#yourNumber').text(counter);
+
+      if (counter == numberToGuess){
+        alert('You won!!!!');
+        NumberReset();
+
+      }else if( counter > numberToGuess){
+        alert('You lost!');
+         NumberReset();
+      }
+    });
+
+  });
---
+++ assets/javascript/game2.js
@@ -0,0 +1,64 @@
+
+function setup () {
+
+    var numberToGuess = Math.floor(Math.random() * 120) + 19;
+
+    var numbersOnCrystals = [];
+
+    var counter = 0;
+
+    var numbers = [10, 5, 3, 7];
+
+    function shuffle(a) {
+        var j, x, i;
+        for (i = a.length; i; i--) {
+            j = Math.floor(Math.random() * i);
+            x = a[i - 1];
+            a[i - 1] = a[j];
+            a[j] = x;
+        }
+    }   
+
+    shuffle(numbers);
+    console.log(numbers);
+  }
+
+  function loadImages () {
+
+    $('#number').text(numberToGuess);
+
+    for (var i=0; i< numbers.length; i++){
+
+      var imageCrystal = $('<img>');
+
+      imageCrystal.attr('data-num', numbers[i]);
+
+      imageCrystal.attr('src', 'http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg');
+
+      imageCrystal.attr('alt', 'crystals');
+
+      imageCrystal.addClass('crystalImage');
+
+      $('#crystals').append(imageCrystal);
+    }
+
+    $('.crystalImage').on('click', function(){
+      counter = counter + parseInt($(this).data('num'));
+
+      $('#yourNumber').text(counter);
+
+      if (counter == numberToGuess){
+        alert('You won!!!!');
+      }else if( counter > numberToGuess){
+        alert('You lost!');
+      }
+    });
+
+    }
+
+
+  $( document ).ready(function() {
+    loadImages();
+    setup ();
+
+  }
